93391c119ce240616eec0438ad9d43965188cbd8
Humphrey Obuobi <obuobhum@gmail.com>
2016-10-20 01:38:22 -0400

    better anagram algorithm

--- js/main.js
+++ js/main.js
@@ -4,12 +4,42 @@ function isAnagram(str1,str2){
   sum2 = 0
   str1 = stripSpaces(str1.toLowerCase());
   str2 = stripSpaces(str2.toLowerCase());
+  var charMap = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 5,
+    'e': 7,
+    'f': 11,
+    'g': 13,
+    'h': 17,
+    'i': 19,
+    'j': 23,
+    'k': 29,
+    'l': 31,
+    'm': 37,
+    'n': 41,
+    'o': 43,
+    'p': 47,
+    'q': 53,
+    'r': 59,
+    's': 61,
+    't': 67,
+    'u': 71,
+    'v': 73,
+    'w': 79,
+    'x': 83,
+    'y': 91,
+    'z': 97
+  }
   if(str1.length != str2.length)
     return false
   else {
     for(x = 0; x<str1.length; x++){
-      sum1 *= str1.charCodeAt(x)
-      sum2 *= str2.charCodeAt(x)
+      if(str1.charCodeAt(x) <= 122 && str1.charCodeAt(x) >= 97)
+        sum1 *= charMap[str1[x]]
+      if(str1.charCodeAt(x) <= 122 && str1.charCodeAt(x) >= 97)
+        sum2 *= charMap[str2[x]]
     }
     if(sum1 == sum2)
       return true
