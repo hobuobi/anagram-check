bf4b345b8fb2211dedef005d44e22a9daead7a3e
Humphrey Obuobi <obuobhum@gmail.com>
2016-10-20 23:57:59 -0400

    fixed anagram code

--- js/main.js
+++ js/main.js
@@ -1,46 +1,53 @@
+var charMap = {
+  'a': 1,
+  'b': 2,
+  'c': 3,
+  'd': 5,
+  'e': 7,
+  'f': 11,
+  'g': 13,
+  'h': 17,
+  'i': 19,
+  'j': 23,
+  'k': 29,
+  'l': 31,
+  'm': 37,
+  'n': 41,
+  'o': 43,
+  'p': 47,
+  'q': 53,
+  'r': 59,
+  's': 61,
+  't': 67,
+  'u': 71,
+  'v': 73,
+  'w': 79,
+  'x': 83,
+  'y': 91,
+  'z': 97
+}
+
 function isAnagram(str1,str2){
   // s.charCodeAt(0)
-  sum1 = 0
-  sum2 = 0
+  sum1 = 1
+  sum2 = 1
   str1 = stripSpaces(str1.toLowerCase());
   str2 = stripSpaces(str2.toLowerCase());
-  var charMap = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 5,
-    'e': 7,
-    'f': 11,
-    'g': 13,
-    'h': 17,
-    'i': 19,
-    'j': 23,
-    'k': 29,
-    'l': 31,
-    'm': 37,
-    'n': 41,
-    'o': 43,
-    'p': 47,
-    'q': 53,
-    'r': 59,
-    's': 61,
-    't': 67,
-    'u': 71,
-    'v': 73,
-    'w': 79,
-    'x': 83,
-    'y': 91,
-    'z': 97
-  }
+
   if(str1.length != str2.length)
     return false
   else {
-    for(x = 0; x<str1.length; x++){
+    for(var x = 0; x<str1.length; x++){
       if(str1.charCodeAt(x) <= 122 && str1.charCodeAt(x) >= 97)
-        sum1 *= charMap[str1[x]]
+      {
+        console.log(charMap[str1[x]])
+        sum1 *= charMap[(str1[x])]
+      }
+
       if(str1.charCodeAt(x) <= 122 && str1.charCodeAt(x) >= 97)
         sum2 *= charMap[str2[x]]
     }
+    console.log(sum1)
     if(sum1 == sum2)
       return true
     else
@@ -64,8 +71,6 @@ $(document).ready(function(){
   $("input").keyup(function(){
     str1 = $("#str1").val()
     str2 = $("#str2").val()
-    console.log(str1)
-    console.log(str2)
     indicate(isAnagram(str1,str2))
   })
 
